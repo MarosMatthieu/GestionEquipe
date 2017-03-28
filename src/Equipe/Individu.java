c09524f8f879f0febba66bcd9878245f00928d39
15marosm <mmaroswork@gmail.com>
2017-03-28 15:52:59 +0200

    getter & setter

--- src/Equipe/Individu.java
+++ src/Equipe/Individu.java
@@ -2,9 +2,45 @@ package Equipe;
 
 public class Individu {
 	
+	
+	/** 
+	 * Proprietees
+	 */
 	private String nom;
 	private String prenom;
 	private int  age;
 	private String shortDesc;
+	
+	
+	/**
+	 * Getter & Setter
+	 */
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getShortDesc() {
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	}
+
+
+
 
 }
